4779ba27048204468921be59cced36a5220fffb4
lingxiao <lingxiao1013@foxmail.com>
2022-12-20 17:48:25 +0800

    第3课spring 使用注解实现DI。@Resource的使用

--- springcourse/ch03/src/main/java/com/lx/bao06/Student.java
+++ springcourse/ch03/src/main/java/com/lx/bao06/Student.java
@@ -1,7 +1,5 @@
 package com.lx.bao06;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 

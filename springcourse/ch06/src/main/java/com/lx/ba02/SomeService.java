eaa9fefbe722125dbd71d541986304927d4dab0b
lingxiao <lingxiao1013@foxmail.com>
2022-12-27 19:24:39 +0800

    第6课动态代理,动态代理的后置通知

---
+++ springcourse/ch06/src/main/java/com/lx/ba02/SomeService.java
@@ -0,0 +1,8 @@
+package com.lx.ba02;
+
+public interface SomeService {
+    void doSome(String name, Integer age);
+    String doOther(String name, Integer age);
+
+    Student doOther2(String name, Integer age);
+}
